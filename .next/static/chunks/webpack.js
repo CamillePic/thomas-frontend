3cabadb2a79ee42fc254f33eb0b1f70e3c2f1887
camillepic <camillejpicard@gmail.com>
2023-09-16 15:14:33 +0200

    dimensions

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7cf89bbed6316ae9"; }
+/******/ 		__webpack_require__.h = function() { return "b65b8d327ff56bbc"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
