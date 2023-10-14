d9ae81192bcc539c53df214ea88e93cdc5e579f5
camillepic <camillejpicard@gmail.com>
2023-10-14 16:16:55 +0200

    almost done - last modifications

--- pages/_app.js
+++ pages/_app.js
@@ -8,12 +8,15 @@ function App({ Component, pageProps }) {
     <>
       <Menu />
       <Head>
+      <link rel="shortcut icon" href="/favicon.ico" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
         <title>Thomas Takada</title>
         <meta
           name="Thomas Takada"
           content="I design objects and fabricate them in limited series by collaborating with local artisans. Find them for sale on this website or contact me for special orders."
         ></meta>
-          <link rel="icon" href="/logoTT.ico"/>
       </Head>
       <Component {...pageProps} />
     </>
