08e4cf068027c18f954a32708189a46e1c771dbb
camillepic <camillejpicard@gmail.com>
2024-05-11 15:56:52 +0200

    tongji

---
+++ pages/Tongji-Expo.js
@@ -0,0 +1,9 @@
+import React from "react";
+import TongjiExpo from "../components/TongjiExpo";
+
+const TongjiExpoPage = () => {
+  return (
+<TongjiExpo />  );
+};
+
+export default TongjiExpoPage;
