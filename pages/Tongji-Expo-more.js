5d0bdfcfbab794c40ee8d344181fb1693fc83393
camillepic <camillejpicard@gmail.com>
2024-05-22 15:23:05 +0200

    added text

--- pages/Tongji-Expo-more.js
+++ pages/Tongji-Expo-more.js
@@ -8,8 +8,24 @@ const TongjiExpoInfo = () => {
       <div className={styles.infoContainer}>
         <h1 className={styles.infoTitle}>Tongji Expo</h1>
         <div className={styles.info}>
-        130m²<br></br><span>Design to tender (ESQ-DCE)<br></br>Built<br></br>2020 - 2021</span><p></p>
-          </div>
+          <span>
+            Since we had a tight budget and only a month for design, fabrication
+            and installation, we exclusively used lightly modified standardized
+            elements. The assemblies are all articulated, allowing flexibility
+            for last minute changes. <br></br>
+            We wanted to use as little material as
+            possible despite the large size of displays (up to 4m50 by 3m high).
+            The minimal design tested the structural limits of the thin, 16mm
+            steel columns.<br></br>
+             Steel footings are cut out of 20mm sheet metal with
+            a water jet. They are quite heavy, providing a low center of gravity
+            despite 3m high displays. <br></br>
+            Different sized tubes are simply pierced
+            so they can fit together. Some are welded to the footings. Some fit
+            steel cables for cross bracing.
+          </span>
+          <p></p>
+        </div>
       </div>
       <div className={styles.gallery}>
         <img src="/gallery/TE_GALLERY_1.jpg" className={styles.imageOne} />
@@ -17,11 +33,9 @@ const TongjiExpoInfo = () => {
         <img src="/gallery/TE_GALLERY_3.jpg" className={styles.imageThree} />
         <img src="/gallery/TE_GALLERY_4.jpg" className={styles.imageThree} />
         <img src="/gallery/TE_GALLERY_5.jpg" className={styles.imageThree} />
-
       </div>
     </div>
   );
-
 };
 
 export default TongjiExpoInfo;
