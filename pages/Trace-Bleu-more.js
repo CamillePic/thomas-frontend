1fb15ac886e67b583b6b62708db5962cf205eb5a
camillepic <camillejpicard@gmail.com>
2024-07-02 11:18:01 +0200

    changement descriptions

--- pages/Trace-Bleu-more.js
+++ pages/Trace-Bleu-more.js
@@ -12,6 +12,8 @@ const TraceBleuInfo = () => {
           Direction of design and fabrication of four models for Architecture Studio’s exhibition at the Centquatre. 
           </span>
           <p></p>
+          "Tracé Bleu" est une méthode menée par l’agence Architecturestudio pour réorienter les réflexions autour des milieux habités au regard de l’urgence climatique. Afin de prolonger et stimuler cette pensée, Architecturestudio et le CENTQUATRE-PARIS ont conçu une exposition associant à l’approche architecturale l'imaginaire de différents artistes.
+          <p></p>
         </div>
       </div>
       <div className={styles.gallery}>
@@ -24,6 +26,10 @@ const TraceBleuInfo = () => {
         <img src="/gallery/TB_GALLERY_7.jpg" className={styles.imageThree} />
         <img src="/gallery/TB_GALLERY_8.jpg" className={styles.imageThree} />
         <img src="/gallery/TB_GALLERY_9.jpg" className={styles.imageThree} />
+        <img src="/gallery/TB_GALLERY_10.jpg" className={styles.imageThree} />
+        <img src="/gallery/TB_GALLERY_11.jpg" className={styles.imageThree} />
+        <img src="/gallery/TB_GALLERY_12.jpg" className={styles.imageThree} />
+        <img src="/gallery/TB_GALLERY_13.jpg" className={styles.imageThree} />
       </div>
     </div>
   );
