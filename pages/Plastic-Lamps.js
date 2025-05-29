0deafe193fe67e8c31e4d2fa02dea930e212399e
camillepic <camillejpicard@gmail.com>
2025-05-29 14:42:53 +0200

    PARKLAMPS

--- pages/Plastic-Lamps.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import PlasticLamps from "../components/PlasticLamps";
-
-const PlasticLampsPage = () => {
-  return (
-<PlasticLamps />  );
-};
-
-export default PlasticLampsPage;
